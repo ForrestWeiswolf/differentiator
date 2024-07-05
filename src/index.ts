fb1f4a53e717b5f41de46f03728b4711bdaa946d
Forrest Wolf <forrest.weiswolf@gmail.com>
2024-07-05 16:00:29 -0400

    Implement differentiation of some constants and linear expressions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-const foo = 1
+const derivative = (expression: string): number => {
+  const variable = expression.match(/[A-Z]/)
 
-export default foo
+  if (!variable?.length) {
+    return 0
+  }
+  else {
+    return parseInt(expression.replace(variable[0], '')) || 1
+  }
+}
+
+export default derivative
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,18 @@
-import foo from '../src/index'
+import jsc from 'jsverify'
+import derivative from '../src/index'
 
-it('is 1', () => {
-  expect(foo).toBe(1)
+describe('derivative', () => {
+  jsc.property(
+    "derivative of a number is 0",
+    jsc.integer(-1000, 1000), i => derivative(`${i}`) === 0
+  )
+
+  it('derivative of a variable is 1', () => {
+    expect(derivative('X')).toBe(1)
+  })
+
+  jsc.property(
+    "derivative of NX is N",
+    jsc.integer(-1000, 1000), i => derivative(`${i}X`) === i
+  )
 })
