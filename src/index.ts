d1d567ea8cf000e368f30bc7ba8d85d98a4875e3
Forrest Wolf <forrest.weiswolf@gmail.com>
2024-07-05 17:56:04 -0400

    Implement helper to sum variables with coefficients

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,25 @@
-const derivative = (expression: string): string => {
+export const sum = (addends: string[]): string => {
+  let variableName = ''
+
+  const constants = addends
+    .filter(a => /^-?\d+(\.\d+)?$/.test(a))
+    .reduce((a, b) => a + parseFloat(b), 0)
+
+  const coeficcients = addends
+    .filter(a => /[A-Z]/.test(a))
+    .reduce((a, b) => {
+      if(b.match(/[A-Z]/)) {
+        variableName = b.match(/[A-Z]/)![0]
+      }
+      const coeficcient = b.match(/\d+(\.\d+)?/)
+      console.log(coeficcient)
+      return a + (coeficcient && coeficcient[0] ? parseFloat(coeficcient[0]) : 1)
+    }, 0)
+
+  return constants > 0 ? String(constants) : `${coeficcients === 1 ? '' : coeficcients}${variableName}`
+}
+
+export const derivative = (expression: string): string => {
   expression = expression.replace(/ \+.+/, '')
   const variable = expression.match(/[A-Z]/)
   const [base, exponent] = expression.split('^')
@@ -8,11 +29,10 @@ const derivative = (expression: string): string => {
   }
   else if (exponent) {
     return `${exponent}${variable[0]}`
-  } else if (base === variable[0]){
+  } else if (base === variable[0]) {
     return '1'
   } else {
     return `${parseInt(base.replace(variable[0], '')) || 0}`
   }
 }
 
-export default derivative
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
 import jsc from 'jsverify'
-import derivative from '../src/index'
+import { derivative, sum } from '../src/index'
 
 describe('derivative', () => {
   jsc.property(
@@ -39,4 +39,30 @@ describe('derivative', () => {
     "derivative of X^C is CX",
     jsc.integer(-1000, 1000), i => derivative(`X^${i}`) === `${i}X`
   )
+})
+
+describe('sum', () => {
+  it('sum of a number is itself', () => {
+    expect(sum(["12"])).toBe("12")
+  })
+
+  it('sum of a variable is itself', () => {
+    expect(sum(["A"])).toBe("A")
+  })
+
+  it('sum of several numbers is just their sum', () => {
+    expect(sum(["12", "1", "1"])).toBe("14")
+  })
+
+  it('sum of A and A is 2A', () => {
+    expect(sum(["A", "A"])).toBe("2A")
+  })
+
+  it('sum of 2A and 4A is 6A', () => {
+    expect(sum(["2A", "4A"])).toBe("6A")
+  })
+
+  it('sum of 2B and 4B is 6B', () => {
+    expect(sum(["2B", "4B"])).toBe("6B")
+  })
 })
