ef225f93532b14f5e19c6fe7b169906ac5ce0fd8
Forrest Wolf <forrest.weiswolf@gmail.com>
2024-07-05 16:27:44 -0400

    Handle some simple exponents

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 const derivative = (expression: string): string => {
+  expression = expression.replace(/ \+.+/, '')
   const variable = expression.match(/[A-Z]/)
+  const [base, exponent] = expression.split('^')
 
   if (!variable?.length) {
     return '0'
   }
-  else {
-    return String(parseInt(expression.replace(variable[0], '')) || 1)
+  else if (exponent) {
+    return `${exponent}${variable[0]}`
+  } else if (base === variable[0]){
+    return '1'
+  } else {
+    return `${parseInt(base.replace(variable[0], '')) || 0}`
   }
 }
 
--- test/index.test.ts
+++ test/index.test.ts
@@ -11,6 +11,10 @@ describe('derivative', () => {
     expect(derivative('X')).toBe("1")
   })
 
+  it('derivative 0X is 0', () => {
+    expect(derivative('0X')).toBe("0")
+  })
+
   jsc.property(
     "derivative of CX is C",
     jsc.integer(-1000, 1000), i => derivative(`${i}X`) === `${i}`
@@ -29,5 +33,10 @@ describe('derivative', () => {
   jsc.property(
     "derivative of C + X is 1",
     jsc.integer(-1000, 1000), i => derivative(`X + ${i}`) === '1'
+  )
+
+  jsc.property(
+    "derivative of X^C is CX",
+    jsc.integer(-1000, 1000), i => derivative(`X^${i}`) === `${i}X`
   )
 })
